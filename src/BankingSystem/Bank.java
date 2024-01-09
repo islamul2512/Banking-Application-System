6941d420077aba362e72e2f47da0f916a0c61ce6
islamul2512 <islamul2512@gmail.com>
2024-01-09 16:23:26 +0530

    Banking

---
+++ src/BankingSystem/Bank.java
@@ -0,0 +1,9 @@
+package BankingSystem;
+
+public interface Bank {
+    public int repoRate = 5;
+    public String addMoney(int money);
+    public String checkBalance(String password);
+    public String withdrawMoney(String password, int money);
+    public double getRateOfInterest(int years);
+}
